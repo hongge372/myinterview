b25413aa867082878b768de9e11d0ad013016011
hongge372 <hongge372@126.com>
2019-04-08 16:37:10 +0800

    static no-staic call

--- cpp/static/staticFuncInClass.cpp
+++ cpp/static/staticFuncInClass.cpp
@@ -2,30 +2,41 @@
 
 class FatherClass{
 public:
-  static void ptlog(){
-    printf("In class father \n");
-  }
+    static void ptlog(){
+        printf("In class father \n");
+    }
 };
 
 class ChildClass{
 public:
-  static void ptlog(){
-    printf("In class child \n");
-  }
+    static void ptlog(){
+        printf("In class child \n");
+            //静态方法不能调用非静态方法，报错
+            //normalBeCall();
+    }
+
+    void normalCall(){
+            //非静态方法可以调用静态方法
+        ptlog();        
+    }
+
+    void normalBeCall(){
+        printf("if ok? \n");
+    }
 };
 
 
-void staticFunc(){
-  FatherClass fath;
-  ChildClass child;
+void callStatic(){
+    FatherClass fath;
+    ChildClass child;
 
-  fath.ptlog();
-  child.ptlog();
+    fath.ptlog();
+    child.ptlog();
 }
 
 
 int main(int argc, char* argv[])
 {
-  staticFunc();
-  return 0;
+    callStatic();
+    return 0;
 }
