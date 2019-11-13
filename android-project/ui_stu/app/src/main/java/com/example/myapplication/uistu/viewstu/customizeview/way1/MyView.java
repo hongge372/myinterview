afa7b41a06d1108fc263f38209267b281897475c
hongge372 <hongge372@126.com>
2019-11-13 15:42:30 +0800

    draw ok

--- android-project/ui_stu/app/src/main/java/com/example/myapplication/uistu/viewstu/customizeview/way1/MyView.java
+++ android-project/ui_stu/app/src/main/java/com/example/myapplication/uistu/viewstu/customizeview/way1/MyView.java
@@ -1,11 +1,17 @@
 package com.example.myapplication.uistu.viewstu.customizeview.way1;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class MyView  extends View{
     public final String TAG = getClass().getName();
+    private int width;
+    private int height;
 
     public MyView(Context context){
         super(context);
@@ -30,6 +36,7 @@ public class MyView  extends View{
                 break;
             case MeasureSpec.EXACTLY: //如果是固定的大小，那就不要去改变它
                 mySize = size;
+                //mySize = defaultSize;
                 break;
         }
         return mySize;
@@ -37,16 +44,33 @@ public class MyView  extends View{
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int width = getMySize(100, widthMeasureSpec);
-        int height = getMySize(100, heightMeasureSpec);
+
+        width = getMySize(100, widthMeasureSpec);
+         height = getMySize(100, heightMeasureSpec);
+        Log.v(TAG, "onMeasure, my width: " + width + "myHeight: " + height);
 
         if(width<height){
             height = width;
         }else {
             width = height;
         }
-
+        //这里很奇怪，这样写可以。如果不写width=275，它就是满屏的。虽然width的值是正确的。
+        width=275;
+        Log.v(TAG, "onMeasure, final----my width: " + width + "myHeight: " + height);
+        widthMeasureSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.getMode(widthMeasureSpec));
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.getMode(heightMeasureSpec));
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        int r = getMeasuredWidth()/2;
+        int centerX = getLeft()+r;
+        int centerY = getTop()+r;
+        Paint paint = new Paint();
+        paint.setColor(Color.GREEN);
+        canvas.drawCircle(centerX, centerY, r, paint);
     }
 }
